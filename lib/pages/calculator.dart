aef283282d15104aac15cdd3e608f0f18ab3a41a
Ajkaushik9045 <104207655+Ajkaushik9045@users.noreply.github.com>
2024-03-29 14:06:48 +0530

    final

--- lib/pages/calculator.dart
+++ lib/pages/calculator.dart
@@ -59,9 +59,9 @@ class _calculatorState extends State<calculator> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                btn("AC", Colors.grey, Colors.black),
-                btn("D", Colors.grey, Colors.black),
-                btn("%", Colors.grey, Colors.black),
+                btn("AC", Colors.purple.shade100, Colors.black),
+                btn("D", Colors.purple.shade100, Colors.black),
+                btn("%", Colors.orange, Colors.black),
                 btn("/", Colors.orange, Colors.black),
               ],
             ),
@@ -71,7 +71,7 @@ class _calculatorState extends State<calculator> {
                 btn("7", Colors.grey, Colors.black),
                 btn("8", Colors.grey, Colors.black),
                 btn("9", Colors.grey, Colors.black),
-                btn("X", Colors.orange, Colors.black),
+                btn("x", Colors.orange, Colors.black),
               ],
             ),
             Row(
@@ -130,7 +130,75 @@ class _calculatorState extends State<calculator> {
       deleteNumber();
       return;
     }
+    if (buttonText == "%") {
+      convertToPercentage();
+      return;
+    }
+    if (buttonText == "=") {
+      calculate();
+      return;
+    }
     append(buttonText);
+  }
+
+  void calculate() {
+    if (num1.isEmpty || num2.isEmpty || operand.isEmpty)
+      return; // Calculate only if all values are present
+    double number1 = double.tryParse(num1) ??
+        0.0; // Parse num1, default to 0.0 if parsing fails
+    double number2 = double.tryParse(num2) ??
+        0.0; // Parse num2, default to 0.0 if parsing fails
+    var result = 0.0;
+    switch (operand) {
+      case "+":
+        result = number1 + number2;
+        break;
+
+      case "-":
+        result = number1 - number2;
+        break;
+
+      case "x":
+        result = number1 * number2;
+        break;
+
+      case "/":
+        if (number2 != 0) {
+          result = number1 / number2;
+        } else {
+          // Handle division by zero
+          // For now, we can set the result to 0.0, but you might want to show an error message
+          result = 0.0;
+        }
+        break;
+      default:
+      // Handle unknown operand (optional)
+    }
+
+    setState(() {
+      num1 = result.toString(); // Update num1 with the result
+      if (num1.endsWith(".0")) {
+        num1 = num1.substring(0, num1.length - 2);
+      }
+      operand = ""; // Reset the operand
+      num2 = ""; // Reset num2
+    });
+  }
+
+  void convertToPercentage() {
+    if (num1.isNotEmpty && num2.isNotEmpty && operand.isNotEmpty) {
+      calculate();
+    }
+    if (operand.isNotEmpty) {
+      return;
+    }
+    final number = double.parse(num1);
+
+    setState(() {
+      num1 = "${(number / 100)}";
+      operand = "";
+      num2 = "";
+    });
   }
 
   void deleteNumber() {
@@ -155,7 +223,9 @@ class _calculatorState extends State<calculator> {
 
   void append(String buttonText) {
     if (buttonText == "." && int.tryParse(buttonText) == null) {
-      if (operand.isNotEmpty && num2.isNotEmpty) {}
+      if (operand.isNotEmpty && num2.isNotEmpty) {
+        calculate();
+      }
       operand = buttonText;
     } else if (num1.isEmpty || operand.isEmpty) {
       if (buttonText == "." && num1.contains(".")) return;
